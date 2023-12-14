9ee048a5e84046c3ea24622edf44de40fd89942e
egeklcsln <ege.klcsln1254@gmail.com>
2023-12-14 18:58:28 +0300

    Post Controller & views

--- app/models/User.php
+++ app/models/User.php
@@ -44,4 +44,11 @@ class User{
             return false;
         }
     }
+
+    public function getUserById($id){
+        $this->db->query('SELECT * FROM users WHERE id = :id');
+        $this->db->bind(':id',$id);
+        $row = $this->db->single();
+        return $row;
+    }
 }
